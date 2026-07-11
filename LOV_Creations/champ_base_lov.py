013afeab0acfc4b77a030c12155e5b05160411c6
Bwlingiant <eric.osborne20@gmail.com>
2026-07-11 11:16:13 -0400

    champ_base_lov update

--- LOV_Creations/champ_base_lov.py
+++ LOV_Creations/champ_base_lov.py
@@ -2,57 +2,58 @@ import os
 import json
 import psycopg
 import sys
+import pprint
+from riotwatcher import LolWatcher, ApiError, RiotWatcher
 
+API_KEY = os.environ['API_KEY_SERVICE']
+lol_watcher = LolWatcher(API_KEY)
+riot_watcher = RiotWatcher(API_KEY)
+lol_region = 'na1'
 
-# db_connection = (
-#     f"dbname={os.environ['DB']} "
-#     f"user={os.environ['POSTGRES_USER']} "
-#     f"password={os.environ['POSTGRES_PASSWORD']} "
-#     f"host={os.environ['PGHOST']} "
-#     f"port={os.environ['PGPORT']}"
-# )
-# conn = psycopg.connect(db_connection)
-
-
-# --- Champion directory ---
-'''This is not used anymore. Need to delve into how this stuff works again. Can I get the data
- from the API and utilize that instead? There has to be a better way to do this than by 
-  doing these hardcoded things. '''
-DATA_DIR = '/home/bwlingiant/python_projects/League-of-Legends-ETL/LOV Creations/ddragon_champions'
-
-# --- SQL Insert Query ---
-INSERT_QUERY = """
-INSERT INTO "lollov".champions_info (
-    id, key, name, title, blurb
+db_connection = (
+    f"dbname={os.environ['DB']} "
+    f"user={os.environ['POSTGRES_USER']} "
+    f"password={os.environ['POSTGRES_PASSWORD']} "
+    f"host={os.environ['PGHOST']} "
+    f"port={os.environ['PGPORT']}"
 )
-VALUES (
-    %(id)s, %(key)s, %(name)s, %(title)s, %(blurb)s
-)
-ON CONFLICT (id) DO UPDATE SET
-    key = EXCLUDED.key,
-    name = EXCLUDED.name,
-    title = EXCLUDED.title,
-    blurb = EXCLUDED.blurb
-"""
+conn = psycopg.connect(db_connection)
+cur = conn.cursor()
 
-# --- Parse and insert data ---
-with conn.cursor() as cur:
-    for filename in os.listdir(DATA_DIR):
-        if filename.endswith(".json"):
-            with open(os.path.join(DATA_DIR, filename), "r", encoding="utf-8") as f:
-                raw = json.load(f)
-                champ_data = list(raw["data"].values())[0]  # Unwrap single champ
+DDRegion = lol_watcher.data_dragon.versions_for_region('na1')['n']
+DDchamps = DDRegion['summoner']
+champs = lol_watcher.data_dragon.champions(DDchamps)
+champ_data = champs['data']
+# pprint.pp(champ_data)
+# print(type(champs))
 
-                payload = {
-                    "id": champ_data["id"],
-                    "key": champ_data["key"],
-                    "name": champ_data["name"],
-                    "title": champ_data["title"],
-                    "blurb": champ_data["blurb"]
-                }
+for n in champ_data:
+    # print(champ_data.keys())
+    # print(champ_data[n])
+    champ_info = champ_data[n]
+    champion_data_dict = {"id": champ_info['id'],
+                          "key" : champ_info['key'],
+                          "name": champ_info['name'],
+                          "title": champ_info['title'],
+                          "blurb": champ_info['blurb']
+                          }
 
-                cur.execute(INSERT_QUERY, payload)
-                print(f"Inserted/Updated: {champ_data['id']}")
+    # --- SQL Insert Query ---
+    INSERT_QUERY = """
+    INSERT INTO "lollov".lol_champions_info (
+        id, key, name, title, blurb
+    )
+    VALUES (
+        %(id)s, %(key)s, %(name)s, %(title)s, %(blurb)s
+    )
+    ON CONFLICT (id) DO UPDATE SET
+        key = EXCLUDED.key,
+        name = EXCLUDED.name,
+        title = EXCLUDED.title,
+        blurb = EXCLUDED.blurb
+    """
+    cur.execute(INSERT_QUERY, champion_data_dict)
+
 
 print("✅ All champions processed.")
 conn.commit()
